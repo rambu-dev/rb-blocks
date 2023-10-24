6c2db3aa4d75ad1eb5cc3a07713f7177853d1d43
rambuvn <vn.nqhung@gmail.com>
2023-10-24 18:55:45 +0700

    update block

--- src/shopee-link/edit.js
+++ src/shopee-link/edit.js
@@ -1,5 +1,18 @@
-import { Button } from '@wordpress/components';
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import {
+	CheckboxControl,
+	RadioControl,
+	TextControl,
+	ToggleControl,
+	SelectControl,
+	PanelBody,
+} from '@wordpress/components';
+
+import { 
+	InnerBlocks, 
+	InspectorControls,
+	useBlockProps,
+	useInnerBlocksProps
+} from '@wordpress/block-editor';
 import { 
 	__experimentalInputControl as InputControl,
 	__experimentalVStack as VStack,
@@ -8,79 +21,56 @@ import {
 } from '@wordpress/components';
 import { useState } from '@wordpress/element';
 
+import defaultTemplate from './template.json';
+
+export const getTemplate = ({ image = '', link = '', message = ''}) => {
+	let templateString = JSON.stringify(defaultTemplate);
+	templateString = templateString
+		.replaceAll('{{link}}', link)
+		.replaceAll('{{message}}', message)
+		.replaceAll('{{image}}', image);
+
+	return JSON.parse(templateString);
+}
+
+const CustomBlocks = ({ template }) => {
+	console.log('rerender');
+	return (
+		<InnerBlocks template={template} />
+	)
+}
+
 const Edit = ({ attributes, setAttributes}) => {
   const [value, setValue] = useState( attributes.url || '' );
+
 
 	const changeHandle = (newValue = '') => {
 		setValue(newValue || '');
 		setAttributes({ url : newValue });
 	}
+	
+	const template = getTemplate({
+		image: "http://placehold.it/80x120?text=img",
+		link: value,
+		message: "hello world"
+	});
 
 	return (
-		<div>
-        <InputControl
-					value={ value }
-					onChange={changeHandle}
-        />
-        {!!value && 
-					<InnerBlocks orientation="horizontal" template={[
-						['core/group', {
-							layout: { 
-								type: 'flex', 
-								flexWrap: 'nowrap',
-								allowSizingOnChildren: true
-							},
-							style: {
-								spacing: {
-									padding: {
-										top: '12px',
-										bottom: '12px',
-										left: '12px',
-										right: '12px'
-									}
-								},
-								border: {
-									radius: '12px'
-								}
-							}
-						}, [
-							['core/image' , {
-								url: 'http://placehold.it/80x100?text=hello',
-								width: '80px',
-								href: value,
-								linkTarget: "_blank",
-								rel: "noreferrer",
-								style: {
-									border: {
-										radius: '12px'
-									}
-								}
-							}],
-							['core/group', {
-								layout: { 
-									type: 'flex', 
-									orientation: 'vertical',
-									allowSizingOnChildren: true
-								},
-								style: {
-									layout: {
-										selfStretch: "fill"
-									},
-								}
-							}, [
-								['core/paragraph', { 
-									content: "<a href='"+value+"'>Hello world</a>", 
-									align: 'left' 
-								}],
-								['core/button', { 
-									text: 'xem',
-									url: value
-								}]
-							]]
-						]]
-					]} />	
+		<>
+				<InspectorControls>
+					<PanelBody title={ 'Settings' }>
+						<InputControl
+							label={"Product Link"}
+							value={ value }
+							onChange={changeHandle}
+						/>
+					</PanelBody>
+					
+				</InspectorControls>
+				{!!value && 
+					<CustomBlocks template={template} />
 				}
-		</div>
+		</>
 	)
 }
 
