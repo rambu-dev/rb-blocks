6f2d6561c78a50100ee0c4b92754bb20273fbb7a
rambuvn <vn.nqhung@gmail.com>
2023-10-27 14:47:45 +0700

    update

--- src/shopee-link/template.php
+++ src/shopee-link/template.php
@@ -1,17 +1,23 @@
 <!-- wp:group {"lock":"all","style":{"spacing":{"padding":{"top":"12px","bottom":"12px","left":"12px","right":"12px"}},"border":{"radius":"12px"}},"layout":{"type":"flex","allowSizingOnChildren":true}} -->
-<div class="wp-block-group" style="border-radius:12px;padding-top:12px;padding-right:12px;padding-bottom:12px;padding-left:12px"><!-- wp:image {"width":"80px","height":"auto","aspectRatio":"1","style":{"border":{"radius":"12px"}}} -->
-<figure class="wp-block-image is-resized has-custom-border"><a href="<?php echo $args['url']; ?>" target="_blank" rel="noreferrer"><img src="<?php echo $args['image'] ?>" alt="" style="border-radius:12px;aspect-ratio:1;width:80px;height:auto"/></a></figure>
-<!-- /wp:image -->
+<div class="wp-block-group" style="border-radius:12px;padding-top:12px;padding-right:12px;padding-bottom:12px;padding-left:12px">
+    <!-- wp:image {"width":"80px","height":"auto","aspectRatio":"1","style":{"border":{"radius":"12px"}}} -->
+    <figure class="wp-block-image is-resized has-custom-border">
+        <a href="<?php echo $args['url']; ?>" target="_blank" rel="noreferrer">
+            <img src="<?php echo $args['image'] ?>" alt="" style="border-radius:12px;aspect-ratio:1;width:80px;height:auto"/>
+        </a>
+    </figure> <!-- /wp:image -->
 
-<!-- wp:group {"style":{"layout":{"selfStretch":"fill"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","flexWrap":"wrap","allowSizingOnChildren":true}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"align":"left"} -->
-<p class="has-text-align-left"><a href="<?php echo $args['url']; ?>" target="_blank" rel="noreferrer" >Your message go here</a></p>
-<!-- /wp:paragraph -->
+    <!-- wp:group {"style":{"layout":{"selfStretch":"fill"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","flexWrap":"wrap","allowSizingOnChildren":true}} -->
+    <div class="wp-block-group">
+        <!-- wp:paragraph {"align":"left"} -->
+        <p class="has-text-align-left"><a href="<?php echo $args['url'] ?>" target="_blank" rel="noreferrer"><?php echo isset($args['message']) ? $args['message'] : 'Your message go here'; ?></a></p>
+        <!-- /wp:paragraph -->
 
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
-<div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="<?php echo $args['url']; ?>" target="_blank" rel="noreferrer">xem</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->
+        <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+        <div class="wp-block-buttons"><!-- wp:button -->
+            <div class="wp-block-button">
+                <a class="wp-block-button__link wp-element-button" href="<?php echo $args['url']; ?>" target="_blank" rel="noreferrer">xem</a>
+            </div> <!-- /wp:button -->
+        </div> <!-- /wp:buttons -->
+    </div> <!-- /wp:group -->
+</div> <!-- /wp:group -->
